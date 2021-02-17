13158fe78e00458d475f9dedd99cb7cd0e598cc0
SHREYA <shreyagopal17@gmail.com>
2021-02-17 14:53:32 +0900

    SHREYA

--- abc087b.py
+++ abc087b.py
@@ -7,10 +7,13 @@ x = int(input()) #50yen
 num_combination = 0 #Total amount
 for n500 in range(0, a+1):
     for n100 in range(0, b+1):
-        for n50 in range(0, c+1):
-            amount = n500*500+n100*100+n50*50
+         #for n50 in range(0, c+1):
+        n50 = (x - n500*500 - n100*100) // 50
+            #amount = n500*500+n100*100+n50*50
             #print(n500, n100, n50, amount)
-            if amount == x :
+            #if amount == x :
+        if n50 <= c and n50>=0:
+                #print(n500, n100, n50)
                 num_combination += 1
 
 print(num_combination)
