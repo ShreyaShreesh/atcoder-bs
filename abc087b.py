fa5f7ba6158eaa4879393624820ec2637dc4d867
SHREYA <shreyagopal17@gmail.com>
2021-02-17 14:29:55 +0900

    SHREYA

---
+++ abc087b.py
@@ -0,0 +1,16 @@
+#ABC087B Coins
+
+a = int(input())
+b = int(input()) # number of 500yen coin
+c = int(input()) #100yen
+x = int(input()) #50yen
+num_combination = 0 #Total amount
+for n500 in range(0, a+1):
+    for n100 in range(0, b+1):
+        for n50 in range(0, c+1):
+            amount = n500*500+n100*100+n50*50
+            #print(n500, n100, n50, amount)
+            if amount == x :
+                num_combination += 1
+
+print(num_combination)
